f6637489dc8debed6d894b02649ddc3f8b125fc9
edouardrolland <edouard.rlld@gmail.com>
2023-11-03 12:52:23 +0100

    neating the code

---
+++ boids.py
@@ -0,0 +1,115 @@
+import numpy as np
+import pygame
+
+
+class Boid():
+
+    def __init__(self, window, margin):
+        self.x = np.random.randint(0, window[0])
+        self.y = np.random.randint(0, window[1])
+        self.vx = float(round(np.random.uniform(-2, 2), 1))
+        self.vy = float(round(np.random.uniform(-2, 2), 1))
+        self.max_speed = 15
+        self.min_speed = 1
+        self.window = window
+        self.margin = margin
+
+    def avoid_edge(self, turnfactor):
+
+        if self.x < 0 or self.x > self.window[0]:
+            self.vx = -self.vx
+        if self.y < 0 or self.y > self.window[1]:
+            self.vy = -self.vy
+        if self.x < self.margin:
+            self.vx += turnfactor
+        if self.x > self.window[0] - self.margin:
+            self.vx -= turnfactor
+        if self.y < self.margin:
+            self.vy += turnfactor
+        if self.y > self.window[1] - self.margin:
+            self.vy -= turnfactor
+
+    def distance_between_boids(self, boid):
+        dx = self.x - boid.x
+        dy = self.y - boid.y
+        return np.sqrt(dx**2 + dy**2)
+
+    def behaviour(self, boids, separation_factor, alignment_factor, cohesion_factor, visual_range):
+        # Separation
+        self.close_dx = 0
+        self.close_dy = 0
+
+        # Alignment
+        self.xvel_avg = 0
+        self.yvel_avg = 0
+        self.neighboring_boids = 0
+
+        # Cohesion
+        self.xpos_avg = 0
+        self.ypos_avg = 0
+
+        for boid in boids:
+
+            if self.distance_between_boids(boid) < visual_range:
+                self.close_dx += self.x - boid.x
+                self.close_dy += self.y - boid.y
+
+            if self.distance_between_boids(boid) < visual_range:
+                self.xvel_avg += boid.vx
+                self.yvel_avg += boid.vy
+                self.neighboring_boids += 1
+                self.xpos_avg += boid.x
+                self.ypos_avg += boid.y
+
+        if self.neighboring_boids > 0:
+
+            self.xpos_avg = self.xpos_avg/self.neighboring_boids
+            self.ypos_avg = self.ypos_avg/self.neighboring_boids
+            self.xvel_avg = self.xvel_avg/self.neighboring_boids
+            self.yvel_avg = self.yvel_avg/self.neighboring_boids
+
+        else:
+
+            self.xpos_avg = self.x
+            self.ypos_avg = self.y
+            self.xvel_avg = self.vx
+            self.yvel_avg = self.vy
+
+        self.vx += self.close_dx * separation_factor + \
+            (self.xvel_avg - self.vx) * alignment_factor + \
+            (self.xpos_avg - self.x) * cohesion_factor
+        self.vy += self.close_dy * separation_factor + \
+            (self.yvel_avg - self.vy) * alignment_factor + \
+            (self.ypos_avg - self.y) * cohesion_factor
+
+    def speed_limit(self):
+
+        if np.abs(self.vx) > 2.5:
+            self.vx = self.vx/np.abs(self.vx) * 2.5
+
+        if np.abs(self.vy) > 2.5:
+            self.vy = self.vy/np.abs(self.vy) * 2.5
+
+        if np.abs(self.vx) < 0.5:
+            self.vx = np.sign(self.vy) * 0.8
+
+        if np.abs(self.vy) < 0.5:
+            self.vy = np.sign(self.vy) * 0.8
+
+    def update(self, boids, separation_factor, alignment_factor, cohesion_factor, visual_range, turnfactor):
+
+        self.behaviour(boids, separation_factor, alignment_factor,
+                       cohesion_factor, visual_range)
+        self.avoid_edge(turnfactor)
+        self.speed_limit()
+        self.x = self.x + self.vx
+        self.y = self.y + self.vy
+
+
+if __name__ == "__main__":
+
+    window = (1000, 1000)
+    visual_range = 20
+    projected_range = 10
+    boid = Boid(window, visual_range)
+    print(boid.x)
---
+++ simulation.py
@@ -0,0 +1,95 @@
+import pygame_widgets
+import pygame
+from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
+import numpy as np
+from boids import Boid
+
+time = 50
+
+class Simulation():
+
+    def __init__(self, window, margin, Number_of_agents):
+
+        pygame.init()
+        self.window = window
+        self.margin = margin
+        self.screen = pygame.display.set_mode(window)
+        pygame.display.set_caption("Predator Prey Simulation") 
+        self.boids =  [Boid(window,margin) for _ in range(Number_of_agents)]
+        self.clock = pygame.time.Clock()  # create pygame clock object        
+
+    def graphic_interface(self):
+
+        x_slider = 80 + self.margin
+        self.separation_slider = Slider(self.screen, x_slider, int(self.margin/6), int(self.window[0]/8), int(self.window[1]/120), min=0, max=0.5, step=0.001, color=(0, 0, 0), handleColour=(255, 0, 0), handleRadius=5, initial=0, handleThickness=0)
+        self.alignment_slider = Slider(self.screen, x_slider, int(self.margin/6) + int(self.window[1]/120) + 10, int(self.window[0]/8), int(self.window[1]/120), min=0, max=0.5, step=0.001, color=(0, 0, 0), handleColour=(255, 0, 0), handleRadius=5, initial=0, handleThickness=0)
+        self.cohesion_slider = Slider(self.screen, x_slider, int(self.margin/6) + 2*int(self.window[1]/120) + 20, int(self.window[0]/8), int(self.window[1]/120), min=0, max=0.5, step=0.001, color=(0, 0, 0), handleColour=(255, 0, 0), handleRadius=5, initial=0, handleThickness=0)
+        self.turning_slider = Slider(self.screen, x_slider, int(self.margin/6) + 3*int(self.window[1]/120) + 30, int(self.window[0]/8), int(self.window[1]/120), min=0, max=1, step=0.001, color=(0, 0, 0), handleColour=(255, 0, 0), handleRadius=5, initial=0, handleThickness=0)
+        self.visual_slider = Slider(self.screen, x_slider, int(self.margin/6) + 4*int(self.window[1]/120) + 40, int(self.window[0]/8), int(self.window[1]/120), min=0, max=200, step=1, color=(0, 0, 0), handleColour=(255, 0, 0), handleRadius=5, initial=0, handleThickness=0)
+
+        self.output_separation = TextBox(self.screen, int(self.margin/2) + int(self.window[0]/4) + 20, int(self.margin/6) - int(self.window[1]/120) + 2, 35, 20, fontSize=15, borderColour=(255, 255, 255), textColour=(0, 0, 0), radius=0, text=str(np.around(self.separation_slider.getValue(), 3)))
+        self.output_alignment = TextBox(self.screen, int(self.margin/2) + int(self.window[0]/4) + 20, int(self.margin/6) + int(self.window[1]/120) + 4, 35, 20, fontSize=15, borderColour=(255, 255, 255), textColour=(0, 0, 0), radius=0, text=str(np.around(self.alignment_slider.getValue(), 3)))
+        self.output_cohesion = TextBox(self.screen, int(self.margin/2) + int(self.window[0]/4) + 20, int(self.margin/6) + 2*int(self.window[1]/120) + 15, 35, 20, fontSize=15, borderColour=(255, 255, 255), textColour=(0, 0, 0), radius=0, text=str(np.around(self.cohesion_slider.getValue(), 3)))
+        self.output_turning = TextBox(self.screen, int(self.margin/2) + int(self.window[0]/4) + 20, int(self.margin/6) + 3*int(self.window[1]/120) + 24, 35, 20, fontSize=15, borderColour=(255, 255, 255), textColour=(0, 0, 0), radius=0, text=str(np.around(self.turning_slider.getValue(), 3)))
+        self.output_visual = TextBox(self.screen, int(self.margin/2) + int(self.window[0]/4) + 20, int(self.margin/6) + 4*int(self.window[1]/120) + 34, 35, 20, fontSize=15, borderColour=(255, 255, 255), textColour=(0, 0, 0), radius=0, text=str(np.around(self.visual_slider.getValue(), 3)))
+
+        self.output_separation.disable()  # Act as label instead of textbox
+        self.output_alignment.disable()  # Act as label instead of textbox
+        self.output_cohesion.disable()  # Act as label instead of textbox
+        self.output_turning.disable()  # Act as label instead of textbox
+        self.output_visual.disable()  # Act as label instead of textbox
+
+        self.font = pygame.font.Font(None, 18)  # Create a font object
+
+
+    def update_animation(self, boids):
+
+        while True:
+
+            self.screen.fill((255, 255, 255))
+            pygame.draw.rect(self.screen, 'black', (self.margin, self.margin, self.window[0] - 2*self.margin, self.window[1] - 2*self.margin))
+            pygame.draw.rect(self.screen, 'white', (self.margin + 2, self.margin + 2, self.window[0] - 2*self.margin - 4, self.window[1] - 2*self.margin - 4))
+            
+            text = self.font.render("Separation", True, (0, 0, 0))
+            self.screen.blit(text, (self.margin, int(self.margin/6)-2))
+
+            text = self.font.render("Separation", True, (0, 0, 0))
+            self.screen.blit(text, (self.margin, int(self.margin/6)-2))
+            separation_factor = self.separation_slider.getValue()
+            
+            text = self.font.render("Alignment", True, (0, 0, 0))
+            self.screen.blit(text, (self.margin, int(self.margin/6)-2 + int(self.window[1]/120) + 10))
+            alignment_factor = self.alignment_slider.getValue()
+            
+            text = self.font.render("Cohesion", True, (0, 0, 0))
+            self.screen.blit(text, (self.margin, int(self.margin/6) + 2*int(self.window[1]/120) + 18))
+            cohesion_factor = self.cohesion_slider.getValue()
+            
+            text = self.font.render("Turning", True, (0, 0, 0))
+            self.screen.blit(text, (self.margin, int(self.margin/6) + 2*int(self.window[1]/120) + 36))
+            turnfactor = self.turning_slider.getValue()
+        
+            text = self.font.render("Visual", True, (0, 0, 0))
+            self.screen.blit(text, (self.margin, int(self.margin/6) + 2*int(self.window[1]/120) + 54))
+            visual_range = self.visual_slider.getValue()
+            
+            for boid in boids:
+                pygame.draw.circle(self.screen, 'red', (boid.x, boid.y), 3)
+                projected_range = 10
+                boid.update(boids, separation_factor, alignment_factor, cohesion_factor, visual_range, turnfactor)
+
+            for events in pygame.event.get():  # loop through all events
+                if events.type == pygame.QUIT:
+                    pygame.quit()
+                    quit()
+
+            self.output_separation.setText(np.around(self.separation_slider.getValue(),3))
+            self.output_alignment.setText(np.around(self.alignment_slider.getValue(), 3))
+            self.output_cohesion.setText(np.around(self.cohesion_slider.getValue(), 3))
+            self.output_turning.setText(np.around(self.turning_slider.getValue(),3))
+            self.output_visual.setText(np.around(self.visual_slider.getValue(),3))
+
+            pygame_widgets.update(events)
+            pygame.display.update()
+            self.clock.tick(time)
